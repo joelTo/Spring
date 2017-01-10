4f099d2b48c85c0dc0551d00764bbcb5c58b2867
joel T <joel.torib@gmail.com>
2017-01-10 17:31:15 +0100

    initialisationpage en static

--- JoelT-pizza-app/src/main/java/fr/pizzeria/controller/PizzaController.java
+++ JoelT-pizza-app/src/main/java/fr/pizzeria/controller/PizzaController.java
@@ -6,9 +6,9 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fr.model.CategoriePizza;
 import fr.model.Pizza;
@@ -28,7 +28,7 @@ public class PizzaController {
 	}
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
-	@RequestBody
+	@ResponseBody
 	public List<Pizza> findAll() {
 		return database.findAll();
 	}
